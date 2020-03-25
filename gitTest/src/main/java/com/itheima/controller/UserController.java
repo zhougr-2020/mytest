b82ec4be0b3a3259063d1b0c2b8c82aa2eec11a0
zhougr-2020 <62637664+zhougr-2020@users.noreply.github.com>
2020-03-25 14:25:01 +0800

    Update UserController.java

--- gitTest/src/main/java/com/itheima/controller/UserController.java
+++ gitTest/src/main/java/com/itheima/controller/UserController.java
@@ -3,5 +3,7 @@ package com.itheima.controller;
 public class UserController {
     public static void main(String[] args) {
         System.out.println("哈哈");
+                System.out.println("成了");
+
     }
 }
